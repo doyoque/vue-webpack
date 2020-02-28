86d1d5c7447007ec12643f00cfffa283c454ee40
doyoque <abiyoga1335@gmail.com>
2020-02-28 14:15:16 +0700

    initial commit

---
+++ src/components/AppMedium.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="title">
+    <h1>Medium Pages</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'AppMedium'
+};
+</script>
+
+<style lang="scss" scoped>
+    .title {
+        h1 {
+            color: red;
+        }
+    }
+</style>
